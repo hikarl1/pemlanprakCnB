13710ae10bfc410bfdc691a1f31a2b0200046c38
hikarl1 <83234547+hikarl1@users.noreply.github.com>
2025-03-05 23:42:05 +0700

    menambahkan komen/penjelasan.java

--- src/car/Heroz.java
+++ src/car/Heroz.java
@@ -10,33 +10,37 @@ package car;
  */
 public class Heroz {
     //field
-    public String Nama;
-    public String Role;
-    public String Hp;
-    public String Attack;
+    public String Nama; // Nama hero
+    public String Role; // Role hero (Fighter, Tank, Marksman, Mage, Assassin, Support)
+    public String Hp; // Hp atau banyaknya darah hero
+    public String Attack; // Damage attack hero
     
-    //method
-    public Heroz(){
+    // constructor default
+    public Heroz(){ 
     }
-    
-    public Heroz(String Nama, String Role, String Hp, String Attack){
+
+    // constructor untuk inisialisasi objek dengan nilai tertentu, contoh: "this.Nama = Nama" berarti menginisialisasi Nama yang ada di field dengan nilai Nama yang ada di parameter, dst
+    public Heroz(String Nama, String Role, String Hp, String Attack){ 
         this.Nama = Nama;
         this.Role = Role;
         this.Hp = Hp;
         this.Attack = Attack;
     }
-    
-    public void infoHeroz(){
-        System.out.println("Nama: " + Nama);
-        System.out.println("Role :" + Role);
-        System.out.println("HP: " + Hp);
-        System.out.println("Attack: " + Attack);
+
+    // method yang menampilkan atribut object
+    public void infoHeroz(){ 
+        System.out.println("Nama: " + Nama); // menampilkan nama hero
+        System.out.println("Role :" + Role); // menampilkan role hero
+        System.out.println("HP: " + Hp); // menampilkan HP hero
+        System.out.println("Attack: " + Attack); // menampilkan attack hero
     }
-    
+
+    // method untuk mencetak suara hero saat bertarung
     public String hiak(){
-        return "Hiakh!!!";
+        return "Hiakh!!!"; // suara hero teriak "Hiakh!!!"
     }
+        // method untuk mencetak suara pukulan hero saat bertarung
     public void bugh(){
-        System.out.println("Bugh! Bugh! Bugh! Bugh");
+        System.out.println("Bugh! Bugh! Bugh! Bugh"); // suara hero memukul/menonjok lawannya, "bugh bugh bugh bugh"
     }
 }
