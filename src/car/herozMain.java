b1a845317b0225d26ea515a0cb35d60b042b4501
hikarl1 <83234547+hikarl1@users.noreply.github.com>
2025-03-06 00:07:03 +0700

    menambahkan komen/penjelasan pada kode.java

--- src/car/herozMain.java
+++ src/car/herozMain.java
@@ -9,24 +9,27 @@ package car;
  * @author Aurel
  */
 import car.Heroz;
-public class herozMain {
+public class HerozMain {
     public static void main(String[] args) {
-        Heroz batman, superman;
-        
+        Heroz batman, superman; //deklarasi variabel hero yaitu, Batman dan Superman
+
+        //instansiasi batman
         batman = new Heroz();
-        batman.Nama = "Batman";
-        batman.Role = "Fighter";
-        batman.Hp = "17500";
-        batman.Attack = "3000";
-        
+        batman.Nama = "Batman"; // isi field Nama dengan Batman, berarti nama hero Batman
+        batman.Role = "Fighter"; // isi field Role dengan Fighter, menunjukkan role Batman adalah Fighter
+        batman.Hp = "17500"; // isi field Hp dengan 17500, HP Batman sebanyak 17500
+        batman.Attack = "3000"; // isi field Attack dengan 3000, serangan Batman memberikan damage sebanyak 3000
+
+        // instansiasi Superman
         superman = new Heroz();
-        superman.Nama = "Superman";
+        superman.Nama = "Superman"; // isi field Nama 
         superman.Role = "Fighter";
         superman.Hp = "20000";
         superman.Attack = "5000";
-                
-        System.out.println("HERO STAT INFORMATION: " + "\n" +
-                           "Nama: " + batman.Nama + "\n"+
+
+        //  menampilkan informasi hero Batman ( menampilkan objek dan atribut - atributnya )
+        System.out.println("HERO STAT INFORMATION: " + "\n" + 
+                           "Nama: " + batman.Nama + "\n"+ // menampilkan nama
                 "Role: " + batman.Role + "\n"+
                 "Hp: " + batman.Hp + "\n"+
                 "Attack: " + batman.Attack + "\n" + "Attack sound: " + batman.hiak()+ "\n" + "Another attack sound: "
@@ -40,7 +43,7 @@ public class herozMain {
                 "Hp: " + superman.Hp + "\n"+
                 "Attack: " + superman.Attack + "\n" + "Attack sound: " + superman.hiak()+ "\n" + "Another attack sound: "
                 );
-        batman.bugh();
+        superman.bugh();
         System.out.println();
     }
 }
