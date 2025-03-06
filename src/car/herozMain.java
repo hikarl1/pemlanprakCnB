ca9eeace7277234dd7e9f4d54c13f2880d7b389a
hikarl1 <83234547+hikarl1@users.noreply.github.com>
2025-03-06 10:49:05 +0700

    Update herozMain.java

--- src/car/herozMain.java
+++ src/car/herozMain.java
@@ -9,7 +9,7 @@ package car;
  * @author Aurel
  */
 import car.Heroz;
-public class HerozMain {
+public class HerozMain { 
     public static void main(String[] args) {
         Heroz batman, superman; //deklarasi variabel hero yaitu, Batman dan Superman
 
@@ -22,26 +22,27 @@ public class HerozMain {
 
         // instansiasi Superman
         superman = new Heroz();
-        superman.Nama = "Superman"; // isi field Nama 
-        superman.Role = "Fighter";
-        superman.Hp = "20000";
-        superman.Attack = "5000";
+        superman.Nama = "Superman"; // isi field Nama dengan Superman, memberi nama hero Superman
+        superman.Role = "Fighter"; // field Role untuk Superman diisi dengan Fighter
+        superman.Hp = "20000"; // Hp superman sebanyak 20000
+        superman.Attack = "5000"; //field Attack diisi dengan 5000, dapat memberikan serangan kepada lawan sebanyak 5000
 
         //  menampilkan informasi hero Batman ( menampilkan objek dan atribut - atributnya )
         System.out.println("HERO STAT INFORMATION: " + "\n" + 
-                           "Nama: " + batman.Nama + "\n"+ // menampilkan nama
-                "Role: " + batman.Role + "\n"+
-                "Hp: " + batman.Hp + "\n"+
-                "Attack: " + batman.Attack + "\n" + "Attack sound: " + batman.hiak()+ "\n" + "Another attack sound: "
+                           "Nama: " + batman.Nama + "\n"+ //menampilkan atribut Nama dari Superman
+                "Role: " + batman.Role + "\n"+ //menampilkan Role dari Superman
+                "HP: " + batman.Hp + "\n"+ //menampilkan banyaknya HP superman
+                "Attack: " + batman.Attack + "\n" + "Attack sound: " + batman.hiak()+ "\n" + "Another attack sound: " //menampilkan damage dan suara bertarung dari superman
                 );
         batman.bugh();
         System.out.println();
         
-        System.out.println("HERO STAT INFORMATION: " + "\n" +
-                           "Nama: " + superman.Nama + "\n"+
-                "Role: " + superman.Role + "\n"+
-                "Hp: " + superman.Hp + "\n"+
-                "Attack: " + superman.Attack + "\n" + "Attack sound: " + superman.hiak()+ "\n" + "Another attack sound: "
+        //  menampilkan informasi hero Superman ( menampilkan objek dan atribut - atributnya )
+        System.out.println("HERO STAT INFORMATION: " + "\n" + 
+                           "Nama: " + superman.Nama + "\n"+ //menampilkan atribut Nama dari Superman
+                "Role: " + superman.Role + "\n"+ // menampilkan Role dari Superman
+                "HP: " + superman.Hp + "\n"+ //menampilkan banyaknya HP superman
+                "Attack: " + superman.Attack + "\n" + "Attack sound: " + superman.hiak()+ "\n" + "Another attack sound: " //menampilkan damage dan suara bertarung dari superman
                 );
         superman.bugh();
         System.out.println();
